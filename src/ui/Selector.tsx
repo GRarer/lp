f85e714bf575a8043ee46eb52c42a2e75dfb49c1
Grace Rarer <gracerarer@gmail.com>
2021-07-16 19:28:39 -0400

    implement list filtering

--- src/ui/Selector.tsx
+++ src/ui/Selector.tsx
@@ -1,18 +1,47 @@
-import { List, ListItem, ListItemIcon, ListItemText,Divider, Typography } from "@material-ui/core";
+import { List, ListItem, ListItemIcon, ListItemText, Typography, InputAdornment, IconButton } from "@material-ui/core";
 import React from "react";
 import { RecordLabel } from "../service/model";
 import AlbumIcon from '@material-ui/icons/Album';
+import ClearIcon from '@material-ui/icons/Clear';
+import FilledInput from '@material-ui/core/FilledInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 type SelectorProps = {
     items: RecordLabel[],
     onSelect: (item: RecordLabel) => void
 };
 
-export class Selector extends React.Component<SelectorProps> {
+type SelectorState = {
+    search: string;
+}
+
+export class Selector extends React.Component<SelectorProps, SelectorState> {
+    constructor(props: Readonly<SelectorProps>) {
+        super(props);
+
+        this.state = {
+            search: ""
+        };
+    }
+
+    private itemMatchesSearch(item: RecordLabel): boolean {
+        const search = this.state.search.toLowerCase();
+        console.log(search);
+        if (search === "") {
+            console.log("no search");
+            return true;
+        }
+        return (
+            item.title?.toLowerCase().includes(search)
+            || item.artist?.toLowerCase().includes(search)
+            || false
+        );
+    }
 
     render(): JSX.Element {
 
-        const rows = this.props.items.map(item => (
+        const rows = this.props.items.filter(item => this.itemMatchesSearch(item)).map(item => (
             <ListItem button divider dense
                 onClick={()=>{this.props.onSelect(item)}}
                 key={item._uuid}
@@ -25,13 +54,29 @@ export class Selector extends React.Component<SelectorProps> {
             </ListItem>
         ));
 
+        console.log(rows);
+
         return (
             <div>
-                <Typography variant="h5" style={{marginTop:"10px"}}>
+                <Typography variant="h5" style={{marginTop:"10px", marginBottom:"5px"}}>
                     Select a label to print
                 </Typography>
+                <FormControl fullWidth variant="filled">
+                    <InputLabel htmlFor="search-control">Search by Title or Artist</InputLabel>
+                    <FilledInput
+                        id="search-control"
+                        value={this.state.search}
+                        onChange={(event)=>{
+                            this.setState({search: event.target.value});
+                        }}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton onClick={()=>{this.setState({search: ""})}}><ClearIcon/></IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
                 <List>
-                    <Divider/>
                     {rows}
                 </List>
             </div>
