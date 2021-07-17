dbcf97de62ab1fa3dffa8c2eeabcd585b7f5e42b
Grace Rarer <gracerarer@gmail.com>
2021-07-17 17:04:29 -0400

    fix indentation in uploader

--- src/ui/Uploader.tsx
+++ src/ui/Uploader.tsx
@@ -32,24 +32,24 @@ export default class Uploader extends React.Component<UploaderProps> {
 
   render() {
     return (
-    <div>
-      <input
-        accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        style={{ display: 'none' }}
-        id="raised-button-file"
-        type="file"
-        ref={this.fileInput}
-        onInput={()=>{this.handleSubmit()}}
-      />
-      <label htmlFor="raised-button-file">
-        <Button
-          variant="contained" component="span"
-          style={{width: "100%", marginTop: "10px"}}
-        >
-          Upload Spreadsheet
-        </Button>
-      </label>
-    </div>
-    );
-  }
+      <div>
+        <input
+          accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+          style={{ display: 'none' }}
+          id="raised-button-file"
+          type="file"
+          ref={this.fileInput}
+          onInput={()=>{this.handleSubmit()}}
+        />
+        <label htmlFor="raised-button-file">
+          <Button
+            variant="contained" component="span"
+            style={{width: "100%", marginTop: "10px"}}
+          >
+            Upload Spreadsheet
+          </Button>
+        </label>
+      </div>
+      );
+    }
   }
